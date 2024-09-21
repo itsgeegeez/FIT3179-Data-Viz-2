7a3b7bfd470d9a52ae32b7f7b03f26955718bb4c
itsgeegeez <79688924+itsgeegeez@users.noreply.github.com>
2024-09-21 16:15:30 +0800

    added files

---
+++ index.js
@@ -0,0 +1,20 @@
+var vg_1 = 'map.vg.json';
+vegaEmbed('#map', vg_1, { actions: false })
+    .then(function (result) {})
+    .catch(console.error);
+var vg_2 = 'chart1.vg.json';
+vegaEmbed('#chart1', vg_2, { actions: false })
+    .then(function (result) {})
+    .catch(console.error);
+vegaEmbed('#chart2', vg_2, { actions: false })
+    .then(function (result) {})
+    .catch(console.error);
+vegaEmbed('#chart3', vg_2, { actions: false })
+    .then(function (result) {})
+    .catch(console.error);
+vegaEmbed('#chart4', vg_2, { actions: false })
+    .then(function (result) {})
+    .catch(console.error);
+vegaEmbed('#chart5', vg_2, { actions: false })
+    .then(function (result) {})
+    .catch(console.error);
